9c9280fdc71be55c458b1f460a3af23a4867e5c0
PShortt07 <aq231513@aquinas.ac.uk>
2025-02-20 09:37:58 +0000

    encrypted passwords, username length limit

--- chessGame/loginPage.cs
+++ chessGame/loginPage.cs
@@ -30,7 +30,37 @@ namespace chessGame
         {
 
         }
-
+        private string hashPassword(string password)
+        {
+            char[] passwordChars = password.ToCharArray();
+            string toHash = "";
+            for (int i = 0; i < passwordChars.Count(); i++)
+            {
+                int code = (int)passwordChars[i];
+                toHash += code;
+            }
+            bool canContinue = true;
+            string hashed = "";
+            do
+            {
+                try
+                {
+                    canContinue = true;
+                    hashed = (long.Parse(toHash) ^ 2).ToString();
+                }
+                catch
+                {
+                    toHash = toHash.Substring(1, toHash.Length - 1);
+                    canContinue = false;
+                }
+            } while (!canContinue);
+            hashed = hashed.Substring(1, hashed.Length - 2);
+            if (hashed.Length > 100)
+            {
+                hashed = hashed.Substring(0, 100);
+            }
+            return hashed;
+        }
         private void logInButton_Click(object sender, EventArgs e)
         {
             //Home: Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\patri\Source\Repos\chessGame2\chessGame\scores.mdf;Integrated Security=True
@@ -41,7 +71,7 @@ namespace chessGame
             string toInsert = "SELECT * FROM [userLogins] WHERE Username = @username AND Password = @password";
             SqlCommand cmd = new SqlCommand(toInsert, scoresCon);
             string username = usernameTextBox.Text;
-            string password = passwordTextBox.Text;
+            string password = hashPassword(passwordTextBox.Text);
             cmd.Parameters.AddWithValue("username", username);
             cmd.Parameters.AddWithValue("password", password);
             var found = cmd.ExecuteScalar();
@@ -64,7 +94,13 @@ namespace chessGame
             string nums = "0123456789";
             string capLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
             string proposedPassword = passwordTextBox.Text;
+            string username = usernameTextBox.Text;
             bool safe = true;
+            if (username.Length > 25)
+            {
+                errorMessage.Text = "Username too long - must be less than 25 characters";
+                safe = false;
+            }
             if (proposedPassword.IndexOfAny(sChars.ToCharArray()) == -1)
             {
                 errorMessage.Text = "Password needs at least one special character";
@@ -84,12 +120,12 @@ namespace chessGame
             {
                 //Home: Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\patri\Source\Repos\chessGame2\chessGame\scores.mdf;Integrated Security=True
                 //College: Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=H:\CS\chessGameSBID\chessGame\Database1.mdf;Integrated Security=True
+                proposedPassword = hashPassword(proposedPassword);
                 SqlConnection scoresCon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\patri\Source\Repos\chessGame2\chessGame\scores.mdf;Integrated Security=True");
                 scoresCon.Open();
                 string toInsert = "INSERT INTO [userLogins] (Username, Password) VALUES (@u, @p)";
                 SqlCommand cmd = new SqlCommand(toInsert, scoresCon);
-                string username = usernameTextBox.Text;
-                string password = passwordTextBox.Text;
+                string password = proposedPassword;
                 cmd.Parameters.AddWithValue("u", username);
                 cmd.Parameters.AddWithValue("p", password);
                 bool goneThrough = true;
